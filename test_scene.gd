7c28d40a7388270d35862bccc707940e967248c7
NJUCSczy <3067887178@qq.com>
2021-07-22 01:33:35 +0800

    add a test room, w/o wall alpha change

--- test_scene.gd
+++ test_scene.gd
@@ -6,7 +6,7 @@ var Identifier="BattleScene"
 
 func _ready():
     Global.set_navigation()
-    Global.PlayerCamera.reset_camera_area(540,260,2560-640,1600-360)
+    Global.PlayerCamera.reset_camera_area(-2000,-2000,2000,2000)
     $NPCs/NPC.init("test_NPC","following")
     $Enermies/Enermy.init("test_enermy",Vector2(-1,0))
     $Enermies/Enermy2.init("test_enermy",Vector2(-1,0))
